9bff8c9aaf933ae6bc517fb8e2cf1dd38375ce15
bobbynew01 <HuaiHai1234@proton.me>
2024-05-08 01:26:35 +0800

    Update database configuration for PostgreSQL

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,16 +1,24 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres", // Adjust dialect based on your database type
-  logging: false,
-  timestamps: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+// Instantiates Sequelize with database parameters
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",  // Change to 'postgres'
+    logging: false,
+    timestamps: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = sequelize;
