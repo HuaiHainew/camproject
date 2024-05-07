b3fbea25efd35d616369a74e964853bea224eb27
bobbynew01 <HuaiHai1234@proton.me>
2024-05-08 01:38:03 +0800

    Fix Sequelize definition and adjust SSL settings

--- config/DBConfig.js
+++ config/DBConfig.js
@@ -1,3 +1,7 @@
+const Sequelize = require('sequelize'); // Ensures Sequelize is properly imported
+require('dotenv').config(); // This will load all the environment variables from your .env file
+
+// Instantiates Sequelize with database parameters from environment variables
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -5,20 +9,21 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "postgres",
+    dialect: 'postgres',  // Specifies that you are using PostgreSQL
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false // CAUTION: only use for testing in development
+        rejectUnauthorized: false // CAUTION: for production you will want this to be true
       }
     },
-    logging: false,
-    timestamps: false,
-    pool: {
+    logging: false,  // Disables logging for production
+    pool: {  // Optional: Configures the connection pool
       max: 5,
       min: 0,
       acquire: 30000,
-      idle: 10000,
+      idle: 10000
     },
   }
 );
+
+module.exports = sequelize;  // Exports the Sequelize instance to be used in other parts of the app
